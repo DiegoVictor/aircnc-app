1acaf22612a1df4893bd662124bd0f433930011f
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-10-03 22:29:52 -0300

    Create/Configure routes

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+
+import Book from './components/pages/Book';
+import List from './components/pages/List';
+import SignIn from './components/pages/SignIn';
+
+export default createAppContainer(
+  createSwitchNavigator({
+    SignIn,
+    Book,
+    List,
+  })
+);
