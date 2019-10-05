63ad66a734abfd40f85f14fbcfb7e0f4a537c8d1
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-10-05 18:55:35 -0300

    Ignore some warnings

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-
+import { YellowBox } from 'react-native';
 import Routes from './src/routes';
+
+YellowBox.ignoreWarnings(['Unrecognized WebSocket']);
 
 export default function App() {
   return <Routes />;
