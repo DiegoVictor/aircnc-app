8a145e98cec72b190441373722072e8103a3ec88
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-10-05 19:00:02 -0300

    Show request answer notification

--- src/components/pages/List/index.js
+++ src/components/pages/List/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { AsyncStorage, ScrollView } from 'react-native';
+import { AsyncStorage, ScrollView, Alert } from 'react-native';
+import socketio from 'socket.io-client';
 
 import Logo from '../../../assets/logo.png';
 import { Container, Brand } from './styles';
@@ -7,6 +8,22 @@ import SpotList from '../../SpotList';
 
 export default function List() {
   const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem('aircnc_user').then(user_id => {
+      const socket = socketio('http://192.168.0.6:3333', {
+        query: { user_id },
+      });
+
+      socket.on('booking_response', booking => {
+        Alert.alert(
+          `Sua reserva em ${booking.spot.company} em ${booking.date} foi ${
+            booking.approved ? 'APROVADA' : 'REJEITADA'
+          }`
+        );
+      });
+    });
+  }, []);
 
   useEffect(() => {
     AsyncStorage.getItem('aircnc_techs').then(technologies => {
