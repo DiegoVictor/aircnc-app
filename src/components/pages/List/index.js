8c4f83928107b6675ee138813adacb63961be83b
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-11-07 17:34:55 -0300

    Fix #10 Configure api url

--- src/components/pages/List/index.js
+++ src/components/pages/List/index.js
@@ -2,20 +2,22 @@ import React, { useState, useEffect } from 'react';
 import { format, parseISO } from 'date-fns';
 import { AsyncStorage, ScrollView, Alert } from 'react-native';
 import socketio from 'socket.io-client';
+import Constants from 'expo-constants';
 
 import Logo from '~/assets/logo.png';
 import { Container, Brand } from './styles';
 import SpotList from '~/components/SpotList';
+
+const { API_URL } = Constants.manifest.extra;
 
 export default function List() {
   const [techs, setTechs] = useState([]);
 
   useEffect(() => {
     AsyncStorage.getItem('aircnc_user').then(user_id => {
-      const socket = socketio('http://192.168.0.6:3333', {
+      const socket = socketio(API_URL, {
         query: { user_id },
       });
-
       socket.on('booking_response', booking => {
         const date = format(parseISO(booking.date), "dd'/'MM'/'yyyy");
         Alert.alert(
--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
+import Constants from 'expo-constants';
+
+const { API_URL } = Constants.manifest.extra;
 
 export default axios.create({
-  baseURL: 'http://192.168.0.6:3333',
+  baseURL: API_URL,
 });
