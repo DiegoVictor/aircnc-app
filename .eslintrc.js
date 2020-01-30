e810fcdb995f901684f06e0f02bf1e7cf0289233
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-01-30 19:51:45 -0300

    Add rule to permit more classes in the same file

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
     'import/prefer-default-export': 'off',
     camelcase: 'off',
     'no-underscore-dangle': 'off',
+    'max-classes-per-file': 'off',
   },
   settings: {
     'import/resolver': {
