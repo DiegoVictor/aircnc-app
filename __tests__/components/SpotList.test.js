e22afa19041ebc8727f92d8c1717e63e898e74ba
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-02-12 20:39:45 -0300

    Prevent Jest to warn with state updates

--- __tests__/components/SpotList.test.js
+++ __tests__/components/SpotList.test.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import MockAdapter from 'axios-mock-adapter';
-import {
-  render,
-  waitForElement,
-  fireEvent,
-} from 'react-native-testing-library';
+import { fireEvent, act } from 'react-native-testing-library';
 import faker from 'faker';
+import { create } from 'react-test-renderer';
 
 import api from '~/services/api';
 import { SpotList } from '~/components/SpotList';
@@ -22,17 +19,23 @@ describe('SpotList component', () => {
 
     api_mock.onGet('/spots', { params: { tech } }).reply(200, [spot, ...rest]);
 
-    const { getByTestId, getByText } = render(
-      <SpotList tech={tech} navigation={{ navigate: jest.fn() }} />
-    );
+    let root;
+    await act(async () => {
+      root = create(
+        <SpotList tech={tech} navigation={{ navigate: jest.fn() }} />
+      );
+    });
 
-    await waitForElement(() => getByTestId(`spot_${spot._id}`));
+    const nodes = root.root
+      .findAllByType('Text')
+      .map(node => node.children.shift());
 
     [spot, ...rest].forEach(s => {
-      expect(getByTestId(`spot_${s._id}`)).toBeTruthy();
-      expect(getByText(s.company)).toBeTruthy();
-      if (spot.price) {
-        expect(getByText(`R$ ${spot.price}/DIA`)).toBeTruthy();
+      expect(root.root.findByProps({ testID: `spot_${s._id}` })).toBeTruthy();
+
+      expect(nodes.includes(s.company)).toBeTruthy();
+      if (s.price) {
+        expect(nodes.includes(`R$ ${s.price}/DIA`)).toBeTruthy();
       }
     });
   });
@@ -46,13 +49,12 @@ describe('SpotList component', () => {
 
     api_mock.onGet('spots', { params: { tech } }).reply(200, [spot, ...rest]);
 
-    const { getByTestId } = render(
-      <SpotList tech={tech} navigation={{ navigate }} />
-    );
+    let root;
+    await act(async () => {
+      root = create(<SpotList tech={tech} navigation={{ navigate }} />);
+    });
 
-    await waitForElement(() => getByTestId(`spot_${spot._id}`));
-
-    fireEvent.press(getByTestId(`spot_book_${spot._id}`));
+    fireEvent.press(root.root.findByProps({ testID: `spot_book_${spot._id}` }));
     expect(navigate).toHaveBeenCalledWith('Book', { id: spot._id });
   });
 });
