63365bc0b38bab929f2080bc84a646e43371df10
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-02-22 00:23:30 -0300

    Update paddings

--- src/components/pages/Bookings/styles.js
+++ src/components/pages/Bookings/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  padding: 30px 0px;
+  padding: 50px 0px 1px;
 `;
 
 export const Brand = styled.Image`
