7186affc072fd678a5653f3cf9e2034f3a6b0db0
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-02-01 18:37:30 -0300

    Allow to set token

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 import { API_URL } from 'react-native-dotenv';
 
-export default axios.create({
+const api = axios.create({
   baseURL: API_URL,
 });
+
+export function setAuthorization(value) {
+  api.defaults.headers.common.Authorization = `Bearer ${value}`;
+}
+
+export default api;
