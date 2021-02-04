bc7ba4c2d5c912badb7e4f27d65a879c949b8304
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-02-04 19:28:37 -0300

    Create socketio mock

---
+++ mocks/socket.io-client.js
@@ -0,0 +1,21 @@
+let callback;
+export const emit = payload => {
+  callback(payload);
+};
+
+export const on = jest.fn((event, cb) => {
+  callback = cb;
+});
+
+export default (url, options) => ({
+  url,
+  options,
+  on,
+  disconnect: jest.fn(),
+  connect: jest.fn(),
+  io: {
+    opts: {
+      query: {},
+    },
+  },
+});
