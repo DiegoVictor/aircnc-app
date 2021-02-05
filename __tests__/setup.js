a625d541f130c77ca183bc8bcec390fc7a54cfba
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-02-05 20:00:00 -0300

    Remove old setup

--- __tests__/setup.js
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import MockAsyncStorage from 'mock-async-storage';
-
-const mockImpl = new MockAsyncStorage();
-jest.mock('@react-native-community/async-storage', () => mockImpl);
