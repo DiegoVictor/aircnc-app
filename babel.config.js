73afd5ab9f529e0f8f77f4d3fb0c5bd83c1f526b
Diego Victor <diegovictorgonzaga@gmail.com>
2022-02-09 00:05:11 -0300

    Move dotenv to plugins

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,7 @@
 module.exports = {
-  presets: [
-    'module:metro-react-native-babel-preset',
-    'module:react-native-dotenv',
-  ],
+  presets: ['module:metro-react-native-babel-preset'],
   plugins: [
+    'module:react-native-dotenv',
     [
       'babel-plugin-root-import',
       {
